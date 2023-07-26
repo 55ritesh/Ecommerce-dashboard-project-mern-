be7f9d34ea1430d0e42511ff7fc46d546f34b09b
Ritesh Kumar <ritesh02072002@gmail.com>
2023-07-26 22:45:54 +0530

    commit 2

--- node-e-dashboard-backend/db/config.js
+++ node-e-dashboard-backend/db/config.js
@@ -1,2 +1,6 @@
 const mongoose= require('mongoose');
-mongoose.connect("mongodb://localhost:27017/e-commerce")
+mongoose.connect("mongodb+srv://user2:JFGqtUHnWKNb1BTh@cluster0.8oiuoqy.mongodb.net/e-commerce")
+
+
+
+// mongodb+srv://user2:JFGqtUHnWKNb1BTh@cluster0.8oiuoqy.mongodb.net/?retryWrites=true&w=majority
